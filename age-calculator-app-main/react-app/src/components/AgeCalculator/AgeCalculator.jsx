6b7388536360c50a7d2eb132a0c9e3e550b415c8
nocomac <andrew.sokolowsky@gmail.com>
2023-05-20 21:35:26 +0200

    make age calculator form verification with react hook form

--- age-calculator-app-main/react-app/src/components/AgeCalculator/AgeCalculator.jsx
+++ age-calculator-app-main/react-app/src/components/AgeCalculator/AgeCalculator.jsx
@@ -7,7 +7,7 @@ import Result from './componentElements/Result'
 
 function BoxContainer () {
     const [result, setResult] = useState(useSelector((state) => state.ageCalculator))
-    const [errorMessage, setErrorMessage] = useState(null)
+    const [dayError, setDayError] = useState('')
     const dispatch = useDispatch()
 
     const countYears = (day, month, year) => {
@@ -18,17 +18,23 @@ function BoxContainer () {
         return {days: diff.getDate() ,months: diff.getMonth(), years: Math.abs(diff.getFullYear() - 1970)}
     }
 
-    const handleSubmit = (e, day, month, year) => {
-        e.preventDefault()
-        const countedAge = countYears(day, month, year)
-        
+    const onSubmit = (data) => {
+        if(new Date(data.year, data.month - 1, data.day).getDate() !== Number(data.day)) {
+            setDayError('dayError')
+            return false;
+        } else {
+            setDayError('')
+        }
+
+        const countedAge = countYears(data.day, data.month, data.year)
+
         dispatch(countAge(countedAge))
         setResult(countedAge)
     }
 
     return (
         <div className="box-container">
-            <Form handleSubmit={handleSubmit} error={errorMessage ? errorMessage : ''} />
+            <Form onSubmit={onSubmit} dayError={dayError ? dayError : ''} setDayError={setDayError} />
             <Result result={result} />
         </div>
     )
--- age-calculator-app-main/react-app/src/components/AgeCalculator/componentElements/Form.jsx
+++ age-calculator-app-main/react-app/src/components/AgeCalculator/componentElements/Form.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
-function Form ({ handleSubmit }) {
+function Form ({ onSubmit, dayError, setDayError }) {
     const [day, setDay] = useState('')
     const [month, setMonth] = useState('')
     const [year, setYear] = useState('')
@@ -15,25 +16,34 @@ function Form ({ handleSubmit }) {
         setYear(e.target.value)
     }
 
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+
     return (
-        <form className="box-form">
+        <form onSubmit={handleSubmit(onSubmit)} className="box-form">
             <div className="box-fields">
                 <label>
                     <p className="box-title">day</p>
-                    <input onChange={(e) => handleDay(e)} type="text" className="box-input" value={day} placeholder="DD" />
+                    <input onChange={(e) => handleDay(e)} {...register('day', {required: true, min: 1, max: 31, onChange: () => setDayError('')}) } type="text" className={errors.day || dayError ? 'box-input box-errorInp' : 'box-input'} defaultValue={day} placeholder="DD" />
+                    {errors?.day?.type === "required" && <p className="box-errorTxt">This field is required</p>}
+                    {errors?.day?.type === "min" || errors?.day?.type === "max" && <p className="box-errorTxt">Must be a valid day</p>}
+                    {dayError === 'dayError' && <p className="box-errorTxt">Must be a valid day</p>}
                 </label>
                 <label>
                     <p className="box-title">month</p>
-                    <input onChange={(e) => handleMonth(e)} type="text" className="box-input" value={month} placeholder="MM" />
+                    <input onChange={(e) => handleMonth(e)} {...register('month', {required: true, min: 1, max: 12}) } type="text" className={errors.month || dayError ? 'box-input box-errorInp' : 'box-input'} defaultValue={month} placeholder="MM" />
+                    {errors?.month?.type === "required" && <p className="box-errorTxt">This field is required</p>}
+                    {errors?.month?.type === "min" || errors?.month?.type === "max" && <p className="box-errorTxt">Must be a valid month</p>}
                 </label>
                 <label>
                     <p className="box-title">year</p>
-                    <input onChange={(e) => handleYear(e)} type="text" className="box-input" value={year} placeholder="YYYY" />
+                    <input onChange={(e) => handleYear(e)} {...register('year', {required: true, min: 1, max: new Date().getFullYear()}) } type="text" className={errors.year || dayError ? 'box-input box-errorInp' : 'box-input'} defaultValue={year} placeholder="YYYY" />
+                    {errors?.year?.type === "required" && <p className="box-errorTxt">This field is required</p>}
+                    {errors?.year?.type === "max" || errors?.year?.type === "min" && (<p>Must be a valid year</p>)}
                 </label>
             </div>
             <div className="box-submit">
                 <div className="box-hr"></div>
-                <button onClick={e => handleSubmit(e, day, month, year)} type="submit"></button>
+                <button type="submit"></button>
             </div>
         </form>
     )
