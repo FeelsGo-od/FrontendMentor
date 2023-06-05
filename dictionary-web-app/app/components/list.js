7d020d3571d0608a889c6afae7f161f687e2033c
nocomac <andrew.sokolowsky@gmail.com>
2023-06-05 21:36:11 +0200

    show "noun" info

--- dictionary-web-app/app/components/list.js
+++ dictionary-web-app/app/components/list.js
@@ -9,7 +9,7 @@ export default function List({ definition, synonyms, antonyms }) {
         { definition }
 
         {synonyms.length !== 0 ? 
-            (<div className="pb-[0.5rem] md:pb-[1rem] pt-[0.4rem] md:pt-[0.6rem]">
+            (<div className="pb-[0.5rem] md:pb-[1rem] pt-[0.15rem] md:pt-[0.6rem]">
                 <span className="text-75 text-[1rem] leading-[1.188rem] md:text-[1.25rem] md:leading-6 mr-[0.66rem]">Synonyms</span>
                 {synonyms.map((synonym) => (
                     <span className={`${inter700.variable} font-inter700 break-all text-violet text-[1rem] leading-[1.188rem] md:text-[1.25rem] md:leading-6 ml-[0.84rem]`}>{synonym}{synonyms.length > 4 && ','}</span>
