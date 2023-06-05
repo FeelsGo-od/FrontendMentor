55626c7def7d5e509f8f6b4796c0cf9003b4b68f
nocomac <andrew.sokolowsky@gmail.com>
2023-06-05 21:49:52 +0200

    fixing nextjs text colors bug

--- dictionary-web-app/app/components/list.js
+++ dictionary-web-app/app/components/list.js
@@ -10,18 +10,18 @@ export default function List({ definition, synonyms, antonyms }) {
 
         {synonyms.length !== 0 ? 
             (<div className="pb-[0.5rem] md:pb-[1rem] pt-[0.15rem] md:pt-[0.6rem]">
-                <span className="text-75 text-[1rem] leading-[1.188rem] md:text-[1.25rem] md:leading-6 mr-[0.66rem]">Synonyms</span>
+                <span className="text-75 text-2D text-[1rem] leading-[1.188rem] md:text-[1.25rem] md:leading-6 mr-[0.66rem]">Synonyms</span>
                 {synonyms.map((synonym) => (
-                    <span className={`${inter700.variable} font-inter700 break-all text-violet text-[1rem] leading-[1.188rem] md:text-[1.25rem] md:leading-6 ml-[0.84rem]`}>{synonym}{synonyms.length > 4 && ','}</span>
+                    <span className={`${inter700.variable} font-inter700 break-all text-violet text-[1rem] leading-[1.188rem] md:text-[1.25rem] md:leading-6 ml-[0.84rem]`}>{synonym}{synonyms.length > 1 && ','}</span>
                 ))}
             </div>) 
         : ''}
         
         {antonyms.length !== 0 ? 
             (<div className="pb-[0.5rem] md:pb-[1rem] pt-[0.4rem] md:pt-[0.6rem]">
-                <span className="text-75 text-[1rem] leading-[1.188rem] md:text-[1.25rem] md:leading-6 mr-[0.66rem]">Antonyms</span>
+                <span className="text-75 text-2D text-[1rem] leading-[1.188rem] md:text-[1.25rem] md:leading-6 mr-[0.66rem]">Antonyms</span>
                 {antonyms.map((antonym) => (
-                    <span className={`${inter700.variable} font-inter700 break-all text-violet text-[1rem] leading-[1.188rem] md:text-[1.25rem] md:leading-6 ml-[0.84rem]`}>{antonym}{antonyms.length > 4 && ','}</span>
+                    <span className={`${inter700.variable} font-inter700 break-all text-violet text-[1rem] leading-[1.188rem] md:text-[1.25rem] md:leading-6 ml-[0.84rem]`}>{antonym}{antonyms.length > 1 && ','}</span>
                 ))}
             </div>) 
         : ''}
--- dictionary-web-app/app/header.js
+++ dictionary-web-app/app/header.js
@@ -27,7 +27,7 @@ export default function Header() {
         </div>
         </nav>
         <div className="nav_search-bar bg-F4 flex items-center rounded-2xl px-6 py-4 mt-6 md:mt-[3.188rem] justify-between">
-        <input type="text" value='keyboard' className={`${inter700.variable} font-inter700 w-[90%] text-[1rem] leading-[1.188rem] md:text-[1.25rem] bg-transparent focus:outline-none`}/>
+        <input type="text" value='keyboard' className={`${inter700.variable} font-inter700 text-2D w-[90%] text-[1rem] leading-[1.188rem] md:text-[1.25rem] bg-transparent focus:outline-none`}/>
         <Image src={'images/icon-search.svg'} width={16} height={16} alt="search word button" className="cursor-pointer" />
         </div>
     </header>
