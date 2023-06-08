5a8440c74494ff255b95234b123b78e24d87f0d7
nocomac <andrew.sokolowsky@gmail.com>
2023-06-08 22:05:48 +0200

    testing localstorage next13 bug solution

--- dictionary-web-app/app/components/searchForm.js
+++ dictionary-web-app/app/components/searchForm.js
@@ -28,8 +28,8 @@ export default function SearchForm() {
 
   return (
     <>
-      <form onSubmit={handleSearching} className={`${error && 'border border-red'} nav_search-bar bg-F4 flex items-center border border-transparent hover:border-violet transition duration-150 ease-in-out rounded-2xl px-6 py-[1.2rem] mt-6 md:mt-[3.188rem] justify-between`}>
-        <input type="text" value={word} onChange={(e) => setWord(e.target.value)} className={`font-bold text-2D w-[90%] text-[1rem] leading-[1.188rem] md:text-[1.25rem] bg-transparent focus:outline-none`} placeholder='Write some word...'/>
+      <form onSubmit={handleSearching} className={`${error && 'border border-red'} nav_search-bar bg-F4 flex items-center border border-transparent hover:border-violet transition duration-150 ease-in-out rounded-2xl px-6 h-[3.1rem] mt-6 md:mt-[3.188rem] justify-between`}>
+        <input type="text" value={word} onChange={(e) => setWord(e.target.value)} className={`font-bold text-2D w-[90%] text-[1rem] h-full leading-[1.188rem] md:text-[1.25rem] bg-transparent focus:outline-none`} placeholder='Write some word...'/>
         <Image onClick={handleSearching} src={'/images/icon-search.svg'} width={16} height={16} alt="search word button" className="cursor-pointer" />
       </form>
       {error ? <span className='text-red text-[0.8rem] md:text-[1.25rem] pt-[0.5rem]'>{error}</span> : ''}
