6ea00d361b2edacaa8703d2183ec4dbccdc85d8d
nocomac <andrew.sokolowsky@gmail.com>
2023-06-13 21:37:22 +0200

    fixing error

--- entertainment-web-app/app/api/getMovies.js
+++
@@ -1,7 +0,0 @@
-// export default async function getMovies(link) {
-//   const res = await fetch(link)
-//   if(!res.ok) {
-//     throw new Error('Failed to fetch data')
-//   }
-//   return res.json()
-// }
