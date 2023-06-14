17a578b011d9f567d4a5e3398fa3f57eafbe2a7d
nocomac <andrew.sokolowsky@gmail.com>
2023-06-14 13:15:55 +0200

    fixing local import error

--- entertainment-web-app/app/(pages)/main/page.js
+++ entertainment-web-app/app/(pages)/main/page.js
@@ -3,13 +3,13 @@ import React from 'react'
 import styles from './main.module.css'
 import Category from '@/app/components/category'
 import SearchInput from '@/app/components/searchInput'
-import movies from '../../data'
+import movies from '@/app/json/data.json'
 
 export default function Main() {
   return (
     <div className={styles.main}>
         <SearchInput />
-        {/* <Category name={'Trending'} category={'isTrending'} movies={movies} type={'Slider'} /> */}
+        <Category name={'Trending'} category={'isTrending'} movies={movies} type={'Slider'} />
         {/* <Category category={'Recommended for you'} movies={{ movies }} type={'Blocks'} /> */}
     </div>
   )
